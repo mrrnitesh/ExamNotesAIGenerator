e095eb00765b581353244d84e23c05b04a619201
Nitesh Kumar <147733127+mrrnitesh@users.noreply.github.com>
2026-06-12 11:06:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://examnotesaigeneratorclient.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
